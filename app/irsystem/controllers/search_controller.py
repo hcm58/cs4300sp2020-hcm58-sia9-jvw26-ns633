38b8aafb314d6fa5b4da38a1176a8de67df36b96
sia9 <sia9@cornell.edu>
2020-04-19 16:11:35 -0400

    changed query retrieval

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from data_code.sent_comment_data import comment_sentiment, state_list
-from data_code.analyze_timeline import *
-
+from data_code.basicinfofromquery import *
 
 project_name = "NJ, Sophia, Jacob, & Haley's Project"
 net_id = "hcm58, sia9, ns633, jvw6"
@@ -23,17 +22,14 @@ net_id = "hcm58, sia9, ns633, jvw6"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
-	states = state_list
-	if query not in states:
-		output_message = "Invalid State Name"
-		data = ""
+	state = getState(query)
+	if state == "notastate":
+		output_message = "Invalid Query"
 	else:
-		state_name = query
-		output_message = state_name
-		data = gen_state_dictionary(query)
+		governor = getGov(state, statedictionary)
+		handle = getHandle(state, statedictionary)
+		party = getParty(state, statedictionary)
+		output_message = state + " - " + governor + " (" +party+  ") - " + handle
 
 
-		# for tweet in state_sentiment:
-
-
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=comment_sentiment)
---
+++ data_code/basicinfofromquery.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+
+import re
+import csv
+
+listofstates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado","Connecticut", "Deleware", "Florida",
+                "Georgia", "Hawaii", "Idaho", "Illinios", "Indiana", "Iowa","Kansas", "Kentucky", "Louisiana", "Maine",
+                "Maryland", "Massachusetts", "MI - Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+                "Nevada", "New Hampshire","New Jersey", "New Mexico", "New York", "North Carolina","North Dakota", "Ohio",
+                "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+                "Utah", "Vermont", "Virgina", "Washington", "West Virginia","Wisconsin", "Wyoming"]
+listofpeople = ["Kay Ivey", "Mike Dunleavy", "Doug Ducey", "Asa Hutchinson", "Gavin Newsom", "Jared Polis", "Ned Lamont",
+                "John Carney", "Ron DeSantis", "Brian Kemp", "David Ige", "Brad Little", "J.B. Pritzker", "Eric Holcomb",
+                "Kim Reynolds", "Laura Kelly", "Andy Beshear", "John Bel Edwards", "Janet Mills", "Larry Hogan",
+                "Charlie Baker", "Gretchen Whitmer", "Tim Walz", "Tate Reeves", "Mike Parson", "Steve Bullock",
+                "Pete Ricketts", "Steve Sisolak", "Chris Sununu", "Phil Murphy", "Michelle Lujan Grisham", "Andrew Cuomo",
+                "Roy Cooper", "Doug Burgum", "Mike DeWine", "Kevin Stitt", "Kate Brown", "Tom Wolf", "Gina Raimondo",
+                "Henry McMaster", "Kristi Noem", "Bill Lee", "Greg Abbott", "Gary Herbert", "Phil Scott", "Ralph Northam",
+                "Jay Inslee", "Jim Justice", "Tony Evers", "Mark Gordon"]
+
+def makedict():
+    with open("data_code/stategovhandleparty.csv", encoding='utf-8') as stategovhandleparty:
+        reader = csv.DictReader(open("data_code/stategovhandleparty.csv", encoding='utf-8'))
+
+
+        bigdict = {}
+        for row in reader:
+            subdict = {}
+            subdict["name"] = row["Name"]
+            subdict["handle"] = row["Twitter"]
+            subdict["party"] = row["Affiliation"]
+            bigdict[row["State"]] = subdict
+    return(bigdict)
+
+def getState(query):
+    if query in listofstates:
+        state = query
+        return state
+    elif query in listofpeople:
+        personindex = listofpeople.index(query)
+        state = listofstates[personindex]
+        return state
+    else:
+        state = "notastate"
+        return state
+
+def getGov(state, dictionary):
+    govname = dictionary[state]["name"]
+    return govname
+
+def getParty(state, dictionary):
+    party = dictionary[state]["party"]
+    return party
+
+def getHandle(state, dictionary):
+    handle = dictionary[state]["handle"]
+    return handle
+
+statedictionary = makedict()
