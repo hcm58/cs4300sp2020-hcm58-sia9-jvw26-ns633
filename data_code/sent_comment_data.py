ef8c5a706a03e82a136912aefe4910973bd9f04b
sia9 <sia9@cornell.edu>
2020-04-20 15:49:00 -0400

    added matplotlib requirement added encoding argument

--- data_code/sent_comment_data.py
+++ data_code/sent_comment_data.py
@@ -39,7 +39,7 @@ word_pattern = re.compile("[\w\-]+") #re looking for 1+ letters or numbers, squa
 
 #make list of states
 state_list = []
-with open("data_code/governors_twitter_info.csv") as info:
+with open("data_code/governors_twitter_info.csv", encoding = 'utf-8') as info:
     reader = csv.reader(info)
     next(reader)
 
@@ -69,7 +69,7 @@ def compile_comment_sent(state_list):
     all_states = {}
     for state in state_list:
         try:
-            with open("data_code/comment_data/" + str(state) + ".csv") as state_file:
+            with open("data_code/comment_data/" + str(state) + ".csv", encoding = 'utf-8') as state_file:
                 reader = csv.reader(state_file)
                 state_dict = {}
                 next(reader)
