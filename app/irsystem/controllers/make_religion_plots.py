44486a4fce10dd66cd8ed1dbf84ad15596028bcd
Haley Mathews <hcm58@cornell.edu>
2020-04-29 13:49:17 -0700

    added title clarification to the pie chart

--- app/irsystem/controllers/make_religion_plots.py
+++ app/irsystem/controllers/make_religion_plots.py
@@ -12,7 +12,7 @@ def make_religion_plot(state, religion_lis):
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
+    plt.title('Of All Religious Words Tweeted, the Percent of Words by Religion')
 
     link = 'app/static/images/' + state + '_religion_plot.png'
 
