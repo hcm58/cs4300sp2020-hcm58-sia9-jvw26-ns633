712faa5156a08551304031abdbb9cd435e5e6303
sia9 <sia9@cornell.edu>
2020-05-01 15:52:43 -0400

    static data tings

---
+++ data_code/getstaticdata.py
@@ -0,0 +1,25 @@
+import csv
+
+statelist = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado","Connecticut", "Delaware", "Florida",
+             "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa","Kansas", "Kentucky", "Louisiana", "Maine",
+             "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+             "Nevada", "New Hampshire","New Jersey", "New Mexico", "New York", "North Carolina","North Dakota", "Ohio",
+             "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+             "Utah", "Vermont", "Virgina", "Washington", "West Virginia","Wisconsin", "Wyoming"]
+
+def getstaticdata(state):
+    with open("data_code/dates4states.csv", encoding='utf-8') as statedates:
+        data = [row for row in csv.reader(statedates)]
+        stateindex = statelist.index(state) + 1
+        listoftuple = []
+        stateofemergency = data[stateindex][1]
+        elementary = data[stateindex][2]
+        nursinghomes = data[stateindex][4]
+        shelterinplace = data[stateindex][5]
+        nonessential = data[stateindex][7]
+        listoftuple.append(("Declared State of Emergency", stateofemergency))
+        listoftuple.append(("K-12 Schools Closed", elementary))
+        listoftuple.append(("Visitors Banned From Nursing Homes", nursinghomes))
+        listoftuple.append(("Shelter-In-Place Declared", shelterinplace))
+        listoftuple.append(("Nonessential Businesses Closed", nonessential))
+        return listoftuple
